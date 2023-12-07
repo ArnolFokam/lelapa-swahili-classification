310a7af4c3d150799e87cd44c091aca070ca28b1
Arnol Fokam <arnolfokam23@gmail.com>
2023-12-07 20:28:07 +0200

    add more data analysis

--- README.md
+++ README.md
@@ -9,13 +9,19 @@
 ### Setup Environment
 
 - Create a virtual environment with the specified Python version (recommended).
+- Install the required packages from [requirements.txt](/requirements.txt).
 
 ### Setup Data
 
-- Download these files and place them in the [data/](/data/) folder.
+- Download these files and place them in the [data/](/data/) folder:
     - `Train.csv`
     - `Test.scv`
     - `SampleSubmission.csv`
 
 
 **Note:** All these steps must be carried out in the project's directory.
+
+
+## Notebooks
+
+- [Data Analysis and Exploration](/notebooks/data_analysis_and_exploration.ipynb)
--- main.py
+++ main.py
@@ -1,2 +1,32 @@
+import random
+import argparse
+
+import numpy as np
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Swahili Text Classification Training and Evaluation')
+    parser.add_argument('--model_name', type=str, help='Timm model name')
+    parser.add_argument('--seed', type=int, default=42, help='Random seed for reproducibility')
+    parser.add_argument('--lr', type=float, default=1e-5, help='Learning rate')
+    parser.add_argument('--epochs', type=int, default=40, help='Number of training epochs')
+    parser.add_argument('--train_batch_size', type=int, default=64, help='Batch size for training')
+    parser.add_argument('--test_batch_size', type=int, default=128, help='Batch size for testing')
+    args = parser.parse_args()
+    return args
+    
+
+def main():
+    
+    # get training and submission parameters
+    args = parse_args()
+    
+    # ensure reproducibility
+    random.seed(SEED)
+    np.random.seed(SEED)
+    
+    
+    
+
 if __name__ == "__main__":
     print("Hello World!")
